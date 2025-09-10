5ab2ac4624b6ba5419bbe7354cddf64c1704028a
RabbaniAryanta <muhammad_aryanta_33rpl@student.smktelkom-mlg.sch.id>
2025-09-10 08:57:11 +0700

    keempat

--- src/siswa/siswa.controller.ts
+++ src/siswa/siswa.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import * as siswaService from './siswa.service';
 
 @Controller('siswa')
